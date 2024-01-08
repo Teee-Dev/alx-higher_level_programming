5660dcc8707c27ce394e55d5ee317a6c22cd4858
Teee-Dev <toloyede45@gmail.com>
2024-01-08 08:47:08 -0800

    5

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,9 @@
+#!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    # Check if idx is negative or out of range
-    if idx < 0 or idx >= len(my_list):
-        return my_list.copy()  # Return a copy of the original list
-
-    # Create a new list with the modification
-    new_list = my_list.copy()
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    new_list = list(my_list)
     new_list[idx] = element
-
     return new_list
